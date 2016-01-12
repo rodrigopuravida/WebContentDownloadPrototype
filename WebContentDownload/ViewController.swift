ce49819c752b8af6268b481b598d212ae743381b
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2016-01-12 09:48:40 -0800

    webview

--- WebContentDownload/ViewController.swift
+++ WebContentDownload/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://stackoverflow.com")!;
+        webView.loadRequest(NSURLRequest(URL: url));
+        
+//        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+//            //when tasks completes
+//            if let urlContent = data {
+//                
+//                
+//                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)!;
+//                
+//                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                    self.webView.loadHTMLString(String(webContent), baseURL: nil);
+//                })
+//                
+//                
+//                
+//                
+//            }
+//        }
+//        
+//        task.resume();
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
